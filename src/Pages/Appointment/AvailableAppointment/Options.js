ecd2a5565a73753941518a00866165b7aebe125b
tonmoy <tonmoy3673@gmail.com>
2023-01-29 22:11:57 +0600

    added prices

--- src/Pages/Appointment/AvailableAppointment/Options.js
+++ src/Pages/Appointment/AvailableAppointment/Options.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const Options = ({ options,setTreatment}) => {
-    const { name, slots } = options;
+    const { name, slots,price } = options;
 
     return (
         <div className="card shadow-xl">
@@ -10,6 +10,7 @@ const Options = ({ options,setTreatment}) => {
                 <h2 className="text-lg text-primary font-bold text-center">{name}</h2>
                 <p >{slots.length>0 ? slots[0]:'Please Try Another Day'}</p>
                 <p className='font-semibold'>{slots.length} {slots.length >1 ? 'spaces':'space'} Available</p>
+                <p className='font-semibold text-error'>Price: <span>{price}$</span></p>
                 <div className="card-actions mx-auto">
                     <label 
                     disabled={slots.length===0}
