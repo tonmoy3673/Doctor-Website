68fbee2306c56fe55ce78bc45d1386f917b52aac
tonmoy <tonmoy3673@gmail.com>
2023-02-16 00:34:10 +0600

    install stripe

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Payment = () => {
-    const {booking}=useLoaderData();
+    const booking=useLoaderData();
+    console.log(booking);
+    const {treatment,slot,price,appointmentDate}=booking;
+
     return (
-        <div>
-            <h1>Payment</h1>
+        <div className='py-10'>
+            <h3 className='text-3xl text-primary font-semibold text-center py-5'>Payment for {treatment}</h3>
+            <p className='text-center text-xl py-3'>Please pay <span className='text-red-600 font-bold'>{price}$</span> for your appointment on <span className='font-semibold'>{appointmentDate}</span> at <span className='font-semibold'>{slot}</span></p>
         </div>
     );
 };
