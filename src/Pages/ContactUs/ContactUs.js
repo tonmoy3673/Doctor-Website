dfe54a2120c11c5777a65e70a8f2427c41de6838
tonmoy <tonmoy3673@gmail.com>
2023-03-01 00:41:02 +0600

    modify navbar

--- src/Pages/ContactUs/ContactUs.js
+++ src/Pages/ContactUs/ContactUs.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Contact from '../Home/Home/Contact/Contact';
-import Footer from '../Home/Share/Footer/Footer';
-import Navbar from '../Home/Share/Navbar/Navbar';
+
 
 const ContactUs = () => {
     return (
--- src/Pages/Home/Share/Navbar/Navbar.js
+++ src/Pages/Home/Share/Navbar/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
 
     <li><Link to='/home'>Home</Link></li>
     <li><Link to='/appointment'>Appointment</Link></li>
-    <li><Link to='/about'>About</Link></li>
+    
 
     <li><Link to='/contact'>Contact Us</Link></li>
     {
